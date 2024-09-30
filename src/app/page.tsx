77794b6414ba236e425ee198b47e877d41cbcf1b
Cedric Franz M Candido <cedricfranzmcandido@Cedrics-Mac-mini.local>
2024-09-30 14:59:28 +0800

    protect route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+'use client'
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+
 
 const page = () => {
+
+    const router = useRouter()  
+
+    useEffect(() => {
+    const username = localStorage.getItem("username");
+    const password = localStorage.getItem("password");
+
+    if (!username && !password) {
+      router.push("/sign-in"); 
+    }
+    else{
+       router.push("/admin/dashboard"); 
+    }
+  }, [router]);
   return <div>CLIENT PAGE</div>;
 };
 
